bab5acc720603c1449f9ad475f868a464a8c4a32
derDere <phillip@deremer.de>
2025-11-03 02:53:52 +0100

    Clean up app initialization: remove unused Task import and delete commented debug/test lines

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 
 import flet as ft
 
-from task_manager import TaskManager, Task
+from task_manager import TaskManager
 from organize_page import Organizer
 from tasks_page import Tasks
 from calendar_page import Calendar
@@ -18,8 +18,6 @@ class App:
         self.calendar_page = Calendar(self, self.task_manager)
         self.nav_bar = NavBar(self, self.organize_page, self.tasks_page, self.calendar_page)
 
-        #self.task_manager.add_task(Task("Test Task", "This is a test task", "2024-12-31", False, 1))
-
     def start(self):
 
         self.page.title = "Task Helper"
@@ -27,10 +25,6 @@ class App:
         self.page.window.center()
 
         self.page.scroll=ft.ScrollMode.AUTO
-
-        #self.page.window.alignment = ft.alignment.center
-
-        #self.page.theme_mode = ft.ThemeMode.LIGHT
 
         self.page.theme = ft.Theme(
             color_scheme_seed=ft.Colors.PURPLE,
